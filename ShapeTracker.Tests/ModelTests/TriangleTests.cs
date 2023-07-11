ccd28eae7036563d0081a9c6f5e1c775b91089d0
Jason E. Church <elijahchurch@gmail.com>
2023-07-10 19:50:44 -0700

    pass final two tests for CheckType method

--- ShapeTracker.Tests/ModelTests/TriangleTests.cs
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -104,5 +104,27 @@ namespace ShapeTracker.Tests
         Assert.AreEqual("scalene triangle", triType);
         }
 
+        [TestMethod]
+        public void CheckType_DeterminesWhenIsosceles_String()
+        {
+        // Arrange
+        Triangle isoscelesTri = new Triangle(5, 4, 4);
+        // Act
+        string triType = isoscelesTri.CheckType();
+        // Assert
+        Assert.AreEqual("isosceles triangle", triType);
+        }
+
+        [TestMethod]
+        public void CheckType_DeterminesWhenEquilateral_String()
+        {
+        // Arrange
+        Triangle equiTri = new Triangle(4, 4, 4);
+        // Act
+        string triType = equiTri.CheckType();
+        // Assert
+        Assert.AreEqual("equilateral triangle", triType);
+        }
+
     }
 }
